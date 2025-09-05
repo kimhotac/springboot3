ebdfaf4969be61d31351911bc88e7c3a4d8f99af
kimhotac <114737603+kimhotac@users.noreply.github.com>
2025-09-05 12:42:45 +0900

    Update hello endpoint response message
    Changed the return string of the hello endpoint to 'I WANT EAT CURRY CUTTLET!!' for a new greeting message.

--- springboot-app3/src/main/java/com/example/demo/UserController2.java
+++ springboot-app3/src/main/java/com/example/demo/UserController2.java
@@ -18,7 +18,7 @@ public class UserController2 {
 
     @GetMapping("/")
     public String hello() {
-        return "Spring Boot! START PAGE2";
+        return "I WANT EAT CURRY CUTTLET!!";
     }
 
     @GetMapping("/mysql")
